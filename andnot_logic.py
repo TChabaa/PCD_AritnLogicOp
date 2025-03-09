f464ed7ec76aed652ec1c1721ac38f2163025cc1
Rafi Prathama <164721768+prathamaarafi@users.noreply.github.com>
2025-03-09 20:18:26 +0700

    Update NAND Logic

--- andnot_logic.py
+++ andnot_logic.py
@@ -2,13 +2,6 @@ import cv2
 import numpy as np
 
 def and_not_operation(image_path1, image_path2):
-    """
-    Perform AND-NOT operation on two preprocessed grayscale images.
-    
-    :param image_path1: Path to the first input image (A).
-    :param image_path2: Path to the second input image (B).
-    :return: Image after applying AND-NOT operation.
-    """
     # Read both images as grayscale
     image1 = cv2.imread(image_path1, cv2.IMREAD_GRAYSCALE)
     image2 = cv2.imread(image_path2, cv2.IMREAD_GRAYSCALE)
@@ -23,10 +16,10 @@ def and_not_operation(image_path1, image_path2):
     if image1.shape != image2.shape:
         image2 = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
     
-    # Apply NOT operation on the second image
-    not_image2 = cv2.bitwise_not(image2)
+    # Apply AND operation on the both image
+    and_result = cv2.bitwise_and(image1, image2)
     
-    # Apply AND operation between image1 and NOT image2
-    and_not_result = cv2.bitwise_and(image1, not_image2)
+    # Perform NOT operation on the AND result
+    and_not_result = cv2.bitwise_not(and_result)
     
     return and_not_result
